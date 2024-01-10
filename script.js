b66595bfe4ae42181f00c7ef590da33517e0d665
Ayushi-Rahane <ayushirahane2021@gmail.com>
2024-01-10 11:09:32 +0530

    c

--- script.js
+++ script.js
@@ -192,3 +192,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+//SmoothScrolling
+
+function smoothScroll(targetId){
+    const targetElement = document.getElementById(targetId);
+    targetElement.scrollIntoView({behavior:'smooth'});
+}
